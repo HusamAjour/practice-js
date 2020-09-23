8aab01fc00085233b344006d57ba0e6a838ba02a
HusamAjour <husamaj278@gmail.com>
2020-09-23 11:32:03 +0300

    add and practise functions

--- js/add-content.js
+++ js/add-content.js
@@ -1,8 +1,9 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+function greetings() {
+  var today = new Date();
+  var hourNow = today.getHours();
+  var greeting;
 
-if (hourNow > 18) {
+  if (hourNow > 18) {
     greeting = 'Good evening, Class!';
   } else if (hourNow > 12) {
     greeting = 'Good afternoon, Class!';
@@ -11,4 +12,22 @@ if (hourNow > 18) {
   } else {
     greeting = 'Something went wrong!';
   }
-document.write('<h3>'+greeting+'</h3>');
+  document.write('<h3>' + greeting + '</h3>');
+}
+
+greetings();
+var view;
+var modelView = function () {
+
+  var model = prompt("What would you like to see? please type \"house\" to see a house and \"hotel\" to see a hotel");
+
+  if (model == "hotel") {
+    view = "<img src=\"images/hotel.png\">";
+  } else if (model == "house") {
+    view = "<img src=\"images/house.png\">";
+  } else {
+    view = "Something went wrong";
+  }
+  document.write('<p>' + view + '</p');
+}
+modelView();
